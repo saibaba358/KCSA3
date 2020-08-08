c3c0bbeb1795f333c262b9134b1e9c3e99b51887
SAIBABA <SAIBABA@LAPTOP-GD7C6DSE>
2020-08-08 15:41:15 +0530

    First commit

---
+++ Actitime/src/test/java/com/actitime/timetrack/First.java
@@ -0,0 +1,12 @@
+package com.actitime.timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class First {
+
+	@Test
+	public void runTest() {
+		Reporter.log("sai",true);
+	}
+}
